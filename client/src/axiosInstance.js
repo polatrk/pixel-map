061cea9212783feb511f0c5be05595c055d8b80c
pithieLS <pierre-alexis.thiebot@epitech.eu>
2024-10-26 18:15:21 +0200

    add remote host for server api req

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://pixel-map-production.up.railway.app',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
