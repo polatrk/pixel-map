c2fe0bd02563afe06046cd6daf623fb5c11048bc
pithieLS <pierre-alexis.thiebot@epitech.eu>
2024-10-27 12:56:21 +0100

    update server url

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://pixel-map-production.up.railway.app',
+    baseURL: 'https://epixel-server.up.railway.app',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
--- client/src/pages/DrawingBoard.js
+++ client/src/pages/DrawingBoard.js
@@ -10,7 +10,7 @@ const DrawingBoard = () => {
 
   useEffect(() => {
     // connect to the wss
-    const socket = new WebSocket('wss://pixel-map-production.up.railway.app')
+    const socket = new WebSocket('wss://epixel-server.up.railway.app')
 
     // init variables
     const canvas = document.querySelector("#drawing-board")
