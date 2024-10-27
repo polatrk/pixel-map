6e7fd8a7996abbad2571208131615706cd72e09c
pithieLS <pierre-alexis.thiebot@epitech.eu>
2024-10-27 16:16:04 +0100

    attempt to fix railway .env.production detection

--- client/build.js
+++
@@ -1,6 +0,0 @@
-// build.js
-require('dotenv').config({ path: '.env.production' });
-const { execSync } = require('child_process');
-
-// Execute the build command
-execSync('react-scripts build', { stdio: 'inherit' });
