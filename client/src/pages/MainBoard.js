ad7d08e2bd37697c5aed52f625fe0f6059e79f45
pithieLS <pierre-alexis.thiebot@epitech.eu>
2024-12-13 10:13:12 +0100

    bug fix after changing scaling to zooming

--- client/src/pages/MainBoard.js
+++ client/src/pages/MainBoard.js
@@ -65,7 +65,7 @@ const MainBoard = () => {
                 ControlZoom(((dy/100)*0.075) * -1, zoomDivRef.current, false)
             else {                                              // case where user is using the trackpad
                 event.preventDefault();
-                // ControlMoveWithTouch(moveDivRef.current, {x: dx, y: dy})
+                ControlMoveWithTouch(moveDivRef.current, {x: dx, y: dy})
             }
         },
         {
@@ -106,8 +106,8 @@ const MainBoard = () => {
                 eventOrigin = e.touches[0];
             }
             
-            if(bIsMouseDown)
-                ControlMoveWithMouse(eventOrigin, moveDivRef.current, clickPosInCanvas)
+            // if(bIsMouseDown)
+            //     ControlMoveWithMouse(eventOrigin, moveDivRef.current, clickPosInCanvas)
 
             // for cell infos
             const cursorPos = getCursorPosInCanvas({pos_x: eventOrigin.clientX, pos_y: eventOrigin.clientY}, canvas)
